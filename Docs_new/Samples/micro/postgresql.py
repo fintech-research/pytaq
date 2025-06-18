493c4e3883c06f49d8b5d4ef132f1750e522eaa4
Vincent Grégoire <vincent.gregoire@gmail.com>
2025-06-18 14:20:31 -0400

    Removed old documentation

--- Docs_new/Samples/micro/postgresql.py
+++
@@ -1,8 +0,0 @@
-from pytaq import TaqDaily
-import wrds
-
-# connecting to WRDS database, use your username instead of 'username', then enter password
-db = wrds.Connection(wrds_username='username')
-
-# creating TaqDaily object
-taq = TaqDaily(method='PostgreSQL', db=db)
